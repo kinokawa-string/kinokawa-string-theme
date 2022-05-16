a5ca9367009049d764ebba60d7ebe3b0adf85184
gw31415 <git@amas.dev>
2022-05-16 11:25:08 +0900

    delete data-progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,18 +3,6 @@ $(window).on('load', function() {
 	$('.preloader').fadeOut(100);
 });
 
-(function($) {
-	'use strict';
-
-	// progress bar
-	$('[data-progress]').each(function() {
-		$(this).css({
-			'bottom': $(this).data('progress')
-		});
-	});
-
-})(jQuery);
-
 window.onscroll = function() {
 	let nav = document.getElementById('navbar');
 	if (document.documentElement.scrollTop > 100) {
